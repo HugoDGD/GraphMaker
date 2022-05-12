1702a8c32483dc928daace7127dc2b6be3e1b74f
HDGD <h.damme@outlook.fr>
2022-05-12 11:02:59 +0200

    The visuals can be set in cmd prompt and small fix in format_conversion

--- plotter.py
+++ plotter.py
@@ -2,10 +2,19 @@ import matplotlib.pyplot as plt
 import decoder as d
 import formater as f
 
-def bodePlot(df):
+def bodePlot(df, visuals=None):
+
+    df.rename(columns={old:new for old,new in zip(df.columns[1:], visuals["labels"])}, inplace=True)
+    print(df.columns)
+
     fig, ax = plt.subplots(figsize=(10,5))
     
+    ax.set_title(visuals["title"])
+    
     ax2 = ax.twinx()
+    ax2.set_ylabel(r"$\angle V_{out}$")
+
+
     
     magnitudes = df.columns[1::2]
     phases = df.columns[2::2]
@@ -13,13 +22,24 @@ def bodePlot(df):
     df.plot(x=0, y=magnitudes, ax=ax, loglog=True)
     df.plot(x=0, y=phases, ax=ax2, linestyle="dotted", logx=True)
 
+    if visuals["x_axis"] != None:
+        ax.set_xlabel(visuals["x_axis"])
+    if visuals["x_lim"] != None:
+        ax.set_xlim(visuals["x_lim"])
+
+    if visuals["y_axis"] != None:
+        ax.set_ylabel(visuals["y_axis"])
+    if visuals["y_lim"] != None:
+        ax.set_ylim(visuals["y_lim"])
 
     ax.legend(loc="upper left")
     ax2.legend(loc="upper right")
     
-    ax2.set_ylim(bottom=0)
+    # ax2.set_ylim(bottom=0)
 
 def plot(df):
+    df.rename(column={old:new for old,new in zip(df.columns, visuals["labels"])}, inplace=True)
+
     df.plot(x=0)
 
 class Plotter:
@@ -38,7 +58,7 @@ class Plotter:
 
         self.formater = f.Formater(formaterFile)
 
-    def plot(self,path):
+    def plot(self,path, visuals=None):
         filename = self.formater.format(path)
 
         if filename == -1:
@@ -56,17 +76,38 @@ class Plotter:
         else:
             return -2
 
-        self.func[type](df)
+        self.func[type](df,visuals)
+
+        plt.show()
 
         return 0
 
 if __name__ == "__main__":
-    import sys
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('f', type=str,
+                        help='The file to plot')
+    parser.add_argument('-t', '--title', type=str, default=None,
+                        help='Title of the figure')
+
+    parser.add_argument('-x', '--x_axis', type=str, default=None,
+                        help='Set the name of the x axis')
+    parser.add_argument('--x_lim', nargs='+', type=float, default=None,
+                        help='Limit of the x axis')
+
+    parser.add_argument('-y','--y_axis', type=str, default=None,
+                        help='Set the name of the y axis')
+    parser.add_argument('--y_lim', nargs='+', type=float, default=None,
+                        help='Limit of the x axis')
+
+    parser.add_argument('-l', '--labels', nargs='+', type=str, default=None,
+                        help='Labels of the signals')
+    
+
+    args = parser.parse_args()
+    
+    visuals = {"title":args.title, "x_axis":args.x_axis, "x_lim":args.x_lim, "y_axis":args.y_axis, "y_lim":args.y_lim, "labels":args.labels}
 
     pelotter = Plotter()
-
-    for file in sys.argv[1:]:
-        print(pelotter.plot(file))
-
-    plt.legend()
-    plt.show()
+    pelotter.plot(args.f, visuals)
